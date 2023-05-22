04ea15ead843844994182cfaa053ef502fd33e8b
dontuty123 <phamtrithuc22022000@gmail.com>
2023-05-22 23:25:26 +0700

    update

--- routes/product.js
+++ routes/product.js
@@ -7,23 +7,18 @@ const {
 
 const router = require("express").Router();
 
-
-router.post("/",verifyTokenAndAdmin, async(req, res)=>{
-    const newProduct= new Product(req.body)
-    try{ 
-        const savedProduct= await newProduct.save();
-        res.status(200).json(savedProduct);
-
-    }catch(err){
-        res.status(500).json(err)
-    }
-    
-})
+router.post("/", verifyTokenAndAdmin, async (req, res) => {
+  const newProduct = new Product(req.body);
+  try {
+    const savedProduct = await newProduct.save();
+    res.status(200).json(savedProduct);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 
 //UPDATE
-router.put("/:id",verifyTokenAndAdmin, async (req, res) => {
-
-
+router.put("/:id", verifyTokenAndAdmin, async (req, res) => {
   try {
     const updatedProduct = await Product.findByIdAndUpdate(
       req.params.id,
@@ -60,29 +55,28 @@ router.get("/find/:id", async (req, res) => {
 
 //GET ALL PRODUCTS
 router.get("/", async (req, res) => {
-    const qNew = req.query.new;
-    const qCategory = req.query.category;
-    try {
-      let products;
-  
-      if (qNew) {
-        products = await Product.find().sort({ createdAt: -1 }).limit(1);
-      } else if (qCategory) {
-        products = await Product.find({
-          categories: {
-            $in: [qCategory],
-          },
-        });
-      } else {
-        products = await Product.find();
-      }
-  
-      res.status(200).json(products);
-    } catch (err) {
-      res.status(500).json(err);
+  const qNew = req.query.new;
+  const qCategory = req.query.category;
+  try {
+    let products;
+
+    if (qNew) {
+      products = await Product.find().sort({ createdAt: -1 }).limit(1);
+    } else if (qCategory) {
+      products = await Product.find({
+        categories: {
+          $in: [qCategory],
+        },
+      });
+    } else {
+      products = await Product.find();
     }
-  });
+    console.log(products);
 
-
+    res.status(200).json(products);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 
 module.exports = router;
